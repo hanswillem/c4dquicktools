db9147a65ed7a9a388c2f63549db5e9bc7ac938b
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-04-12 18:35:08 +0200

    Create glitchObj.py

---
+++ glitchObj.py
@@ -0,0 +1,47 @@
+"""
+Copyright: Hans Willem Gijzel (hanswillemgijzel@gmail.com)
+C4D R16.021
+Name-US:Glitch Obj
+Description-US:Glitches the current scene by exporting it as .obj and randomizing the data of the file
+"""
+
+import c4d
+import random
+
+#set paths, stating the obvious but change these paths to match your machine
+exportedFile = '/Users/hanswillemgijzel/Documents/C4D Glitch OBJ/modelExport.obj'
+glitchedFile = '/Users/hanswillemgijzel/Documents/C4D Glitch OBJ/modelGlitched.obj'
+
+#if alt is pressed when launching the script, open the last saved glithed file
+bc = c4d.BaseContainer()
+if c4d.gui.GetInputState(c4d.BFM_INPUT_KEYBOARD,c4d.BFM_INPUT_CHANNEL,bc):
+    if bc[c4d.BFM_INPUT_QUALIFIER] & c4d.QALT:
+        c4d.documents.MergeDocument(doc, glitchedFile, 1)
+
+#save doc as obj
+c4d.documents.SaveDocument(doc, exportedFile ,c4d.SAVEDOCUMENTFLAGS_0, c4d.FORMAT_OBJEXPORT)
+
+#remove all objects from scene
+c4d.CallCommand(100004766) #select all objects
+for i in doc.GetActiveObjects(1):
+    i.Remove()
+
+#change numbers in obj file
+count = 0
+f = open(exportedFile)
+fn = open(glitchedFile, 'w')
+for l in f:
+    if count % 10 == 0:
+        rn1 = random.choice(range(10))
+        rn2 = random.choice(range(10))
+        l = [rn1 if i == str(rn2) else i for i in l] #pick random numbers and change them in other random numbers
+        #l = [0 if i == 'f' else i for i in l] #remove faces
+        l = [str(i) for i in l]
+    fn.write(''.join(l))
+    count = count + 1
+
+#save file and reopen in c4d (merge with current doc)
+f.close()
+fn.close()
+c4d.documents.MergeDocument(doc, glitchedFile, 1)
+c4d.EventAdd()
