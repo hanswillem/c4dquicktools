bacacf15a4ab1c2df01b66735d248cf8e775923b
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-09-16 12:12:41 +0200

    Update aeFaceTracking.py

--- aeFaceTracking.py
+++ aeFaceTracking.py
@@ -2,7 +2,7 @@
 Copyright: Hans Willem Gijzel (hanswillemgijzel@gmail.com)
 C4D R17.032
 Name-US:AE Face Tracking
-Description-US:Removes general face movement from individual Nulls 
+Description-US:Removes general face movement from individual Nulls
 """
 
 import c4d
@@ -16,61 +16,64 @@ else:
     #make lists with objects
     headObj = allObjs[0]
     childObjs = allObjs[1:]
-    
+
     #get range from user
     maxFrame = doc.GetMaxTime().GetFrame(doc.GetFps())
     txtInput = gui.InputDialog('range', '0-' + str(maxFrame))
     keyFrameRangeStart = int(txtInput.split('-')[0])
     keyFrameRangeEnd = int(txtInput.split('-')[1])
-    
+
     #calculate new keyframes
     for i in range(keyFrameRangeStart, keyFrameRangeEnd + 1):
         #set playhead
         doc.SetTime(c4d.BaseTime(i, doc.GetFps()))
-        
+
         #get head tracks
         hxTrack = headObj.FindCTrack(c4d.DescID(c4d.DescLevel(c4d.ID_BASEOBJECT_POSITION, c4d.DTYPE_VECTOR, 0), c4d.DescLevel(c4d.VECTOR_X, c4d.DTYPE_REAL, 0)))
         hyTrack = headObj.FindCTrack(c4d.DescID(c4d.DescLevel(c4d.ID_BASEOBJECT_POSITION, c4d.DTYPE_VECTOR, 1), c4d.DescLevel(c4d.VECTOR_Y, c4d.DTYPE_REAL, 0)))
-    
+
         #get head curves
         hxCurve = hxTrack.GetCurve()
         hyCurve = hyTrack.GetCurve()
-    
+
         #get head start x,y
         hsx = hxCurve.GetValue(c4d.BaseTime(0, doc.GetFps()), doc.GetFps())
         hsy = hyCurve.GetValue(c4d.BaseTime(0, doc.GetFps()), doc.GetFps())
         ##get head current x,y
         hx = hxCurve.GetValue(c4d.BaseTime(i, doc.GetFps()), doc.GetFps())
         hy = hyCurve.GetValue(c4d.BaseTime(i, doc.GetFps()), doc.GetFps())
-        
+
         headVector = c4d.Vector(hx, hy, 0)
-    
+
         for j in childObjs:
             #get tracks
             xTrack = j.FindCTrack(c4d.DescID(c4d.DescLevel(c4d.ID_BASEOBJECT_POSITION, c4d.DTYPE_VECTOR, 0), c4d.DescLevel(c4d.VECTOR_X, c4d.DTYPE_REAL, 0)))
             yTrack = j.FindCTrack(c4d.DescID(c4d.DescLevel(c4d.ID_BASEOBJECT_POSITION, c4d.DTYPE_VECTOR, 1), c4d.DescLevel(c4d.VECTOR_Y, c4d.DTYPE_REAL, 0)))
-    
+
             #get curves
             xCurve = xTrack.GetCurve()
             yCurve = yTrack.GetCurve()
-    
+
             #get x,y
             x = xCurve.GetValue(c4d.BaseTime(i, doc.GetFps()), doc.GetFps())
             y = yCurve.GetValue(c4d.BaseTime(i, doc.GetFps()), doc.GetFps())
             childVector = c4d.Vector(x, y, 0)
-    
+
             newPos = childVector - headVector
-    
+
             #set x
             nx = newPos[0]
             xKeydict = xCurve.AddKey(c4d.BaseTime(i, doc.GetFps()))
             xKey = xKeydict["key"]
             xKey.SetValue(xCurve, nx)
-            
+
             #set y
             ny = newPos[1]
             yKeydict = yCurve.AddKey(c4d.BaseTime(i, doc.GetFps()))
             yKey = yKeydict["key"]
             yKey.SetValue(yCurve, ny)
-    
+
             j.InsertUnder(headObj)
+            
+    #set the scale of the headObj to 1
+    headObj[c4d.ID_BASEOBJECT_REL_SCALE] = c4d.Vector(1, 1, 1)
