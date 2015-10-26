945c2e846e81ea9ab7516f21dd37fcfa44d8f537
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-10-26 13:15:50 +0100

    Update tracerBuildRig.py

--- tracerBuildRig.py
+++ tracerBuildRig.py
@@ -3,15 +3,19 @@ Copyright: Hans Willem Gijzel (hanswillemgijzel@gmail.com)
 C4D R17.032
 
 Name-US:Tracer Build Rig
-Description-US:Builds a tracer rig from the selected joints
+Description-US:Builds a tracer rig from the selected tracer joints
 """
+
+#The script needs three or more nulls to be selected to work properly. 
+#It builds a rig with controls from the selected nulls using a tracer object.
+#The order in which the nulls are selected before running the script determine the start and end of the hierarchy of the rig
 
 import c4d
 doc.StartUndo()
 
 nulls = doc.GetActiveObjects(2)
     
-#create tracer
+#create tracer object
 tracer = c4d.BaseObject(1018655)
 doc.InsertObject(tracer)
 doc.AddUndo(c4d.UNDOTYPE_NEW, tracer) #*****UNDO*****
@@ -29,7 +33,7 @@ allgp = []
 for i in nulls:
     allgp.append(i.GetMg())
 
-#create hierarchy
+#create hierarchy of nulls
 nullNrs = range(1, len(nulls))
 nullNrs.reverse()
 for i in nullNrs:
@@ -40,7 +44,7 @@ for i in nullNrs:
 for i in range(len(nulls)):
     nulls[i].SetMg(allgp[i])
 
-#rename joints
+#rename nulls
 for i in range(len(nulls)):
     nulls[i].SetName('joint')
 
@@ -60,12 +64,12 @@ doc.AddUndo(c4d.UNDOTYPE_NEW, group) #*****UNDO*****
 #put tracer in group
 tracer.InsertUnder(group)
 
-#create ik chain and goal
+#create ik chain
 iktag = c4d.BaseTag(1019561)
 iktag[c4d.ID_CA_IK_TAG_TIP] = nulls[-1]
 nulls[0].InsertTag(iktag)
 doc.AddUndo(c4d.UNDOTYPE_NEW, iktag) #*****UNDO*****
-
+#create goal
 goal = c4d.BaseObject(5140)
 goal.SetMg(allgp[-1])
 goal[c4d.NULLOBJECT_DISPLAY] = 11
@@ -75,7 +79,6 @@ goal.SetName('goal')
 doc.InsertObject(goal)
 doc.AddUndo(c4d.UNDOTYPE_NEW, goal) #*****UNDO*****
 iktag[c4d.ID_CA_IK_TAG_TARGET] = goal
-
 
 c4d.EventAdd()
 doc.EndUndo()
