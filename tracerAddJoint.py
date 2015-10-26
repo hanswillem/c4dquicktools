c93543eb6dc6a8ea924a2870aa79235cddcd06ec
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-10-26 13:14:40 +0100

    Update tracerAddJoint.py

--- tracerAddJoint.py
+++ tracerAddJoint.py
@@ -6,6 +6,9 @@ Name-US:Tracer Add Joint
 Description-US:Adds a tracer joint
 """
 
+#The script adds a null to the scene and sets it's display type to sphere.'
+#It is meant to work together with the "Tracer Build Rig script" and the "Tracer Build Mesh" script.
+
 import c4d
 doc.StartUndo()
 
@@ -15,7 +18,7 @@ joint[c4d.NULLOBJECT_ORIENTATION] = 1
 joint.SetName('joint')
 doc.InsertObject(joint)
 doc.SetSelection(joint, c4d.SELECTION_NEW)
-doc.AddUndo(c4d.UNDOTYPE_NEW, joint)
+doc.AddUndo(c4d.UNDOTYPE_NEW, joint) #*****UNDO*****
 
 c4d.EventAdd()
 doc.EndUndo()
