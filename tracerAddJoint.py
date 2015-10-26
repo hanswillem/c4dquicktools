3784c4c67d7b4c417e8e0e201f79922ac0ca2126
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-10-26 11:14:16 +0100

    Update tracerAddJoint.py

--- tracerAddJoint.py
+++ tracerAddJoint.py
@@ -7,12 +7,15 @@ Description-US:Adds a tracer joint
 """
 
 import c4d
+doc.StartUndo()
 
-null = c4d.BaseObject(c4d.Onull)
-null[c4d.NULLOBJECT_DISPLAY] = 13
-null[c4d.NULLOBJECT_ORIENTATION] = 1
-null.SetName('joint')
-doc.InsertObject(null)
-doc.SetSelection(null, c4d.SELECTION_NEW)
+joint = c4d.BaseObject(c4d.Onull)
+joint[c4d.NULLOBJECT_DISPLAY] = 13
+joint[c4d.NULLOBJECT_ORIENTATION] = 1
+joint.SetName('joint')
+doc.InsertObject(joint)
+doc.SetSelection(joint, c4d.SELECTION_NEW)
+doc.AddUndo(c4d.UNDOTYPE_NEW, joint)
 
 c4d.EventAdd()
+doc.EndUndo()
