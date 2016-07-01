cd2f5fa772895157c922ef989e082ca39d8459db
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-07-01 10:51:39 +0200

    Add files via upload

---
+++ nom_setupRopes.py
@@ -0,0 +1,51 @@
+"""
+Copyright: Hans Willem Gijzel (hanswillemgijzel@gmail.com)
+C4D R17.048
+
+Name-US:NOM Setup Ropes
+Description-US:Turns selected dynamic splines into dynamic meshes
+"""
+
+
+import c4d
+from c4d import gui
+
+doc.StartUndo() #undo
+
+def setupRopes(n):
+    objs = doc.GetActiveObjects(1)
+    for i in objs:
+        
+        #setup cylinder
+        cyl = c4d.BaseObject(5170)
+        cyl[c4d.PRIM_CYLINDER_RADIUS] = n
+        cyl[c4d.PRIM_CYLINDER_HSUB] = 50        
+        parent = i.GetUp()
+        if parent != None:
+            cyl.InsertUnder(parent)
+        else:
+            doc.InsertObject(cyl)
+        doc.AddUndo(c4d.UNDOTYPE_NEW, cyl) #undo
+        
+        #setup phong tag
+        phongTag = c4d.BaseTag(5612)
+        cyl.InsertTag(phongTag)
+
+        #setup point cache tag
+        cacheTag = c4d.BaseTag(1021302)
+        cyl.InsertTag(cacheTag)
+
+        #setup spline wrap
+        splWrap = c4d.BaseObject(1019221)
+        splWrap[c4d.MGSPLINEWRAPDEFORMER_SPLINE] = i
+        splWrap[c4d.MGSPLINEWRAPDEFORMER_AXIS] = 2
+        splWrap.InsertUnder(cyl)
+
+    c4d.EventAdd()
+    doc.EndUndo() #undo
+
+
+userInput = gui.RenameDialog('thickness')
+if userInput != None:
+    userInput = float(userInput)
+    setupRopes(userInput)
