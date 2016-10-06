ac4e1ab4fe5b3eeaf109b6449d50e359edadf1d4
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-10-06 16:31:22 +0200

    Add files via upload

--- downloadAndInstallScripts.py
+++ downloadAndInstallScripts.py
@@ -1,39 +1,50 @@
 import c4d
+from c4d import gui
 import urllib
 import zipfile
 import os
 import shutil
 
+def main():
 
-# c4d script folder
-scriptFolder = c4d.storage.GeGetC4DPath(1)[:-6] + '/library/scripts/'
+    confirm = gui.QuestionDialog('Download and install scripts from github?')
 
+    if confirm:
 
-# download github repo
-url = 'https://github.com/hanswillem/c4dquicktools/archive/master.zip'
-targetDir = '/Users/gewoonsander/Desktop/'
-target = targetDir + '/master.zip'
-urllib.urlretrieve(url, target)
+        # c4d script folder
+        scriptFolder = c4d.storage.GeGetC4DPath(1)[:-6] + '/library/scripts/'
 
 
-# unzip
-zip_ref = zipfile.ZipFile(target, mode='r')
-zip_ref.extractall(targetDir)
-zip_ref.close()
-extractdDir = '/Users/gewoonsander/Desktop/c4dquicktools-master'
+        # download github repo
+        url = 'https://github.com/hanswillem/c4dquicktools/archive/master.zip'
+        targetDir = '/Users/gewoonsander/Desktop/'
+        target = targetDir + '/master.zip'
+        urllib.urlretrieve(url, target)
 
 
-# copy
-allScriptFiles = [f for f in os.listdir(extractdDir) if os.path.splitext(f)[1] == '.py' or os.path.splitext(f)[1] == '.tif']
-for i in allScriptFiles:
-    fullPath = os.path.join(extractdDir, i)
-    fullPathTarget = os.path.join(scriptFolder, i)
-    shutil.copy(fullPath, fullPathTarget)
+        # unzip
+        zip_ref = zipfile.ZipFile(target, mode='r')
+        zip_ref.extractall(targetDir)
+        zip_ref.close()
+        extractdDir = '/Users/gewoonsander/Desktop/c4dquicktools-master'
 
 
-# clean up
-os.remove(target)
-shutil.rmtree(extractdDir)
+        # copy
+        allScriptFiles = [f for f in os.listdir(extractdDir) if os.path.splitext(f)[1] == '.py' or os.path.splitext(f)[1] == '.tif']
+        for i in allScriptFiles:
+            fullPath = os.path.join(extractdDir, i)
+            fullPathTarget = os.path.join(scriptFolder, i)
+            shutil.copy(fullPath, fullPathTarget)
 
 
-print 'done!'
+        # clean up
+        os.remove(target)
+        shutil.rmtree(extractdDir)
+
+
+        # message
+        c4d.gui.MessageDialog('Scripts downloaded and installed!')
+
+
+if __name__ == '__main__':
+    main()
